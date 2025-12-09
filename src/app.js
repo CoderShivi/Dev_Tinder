d422a02130257d724cfb388098f66fedb9f760bb
Shivani <108396731+CoderShivi@users.noreply.github.com>
2025-12-09 17:40:40 +0530

    mongo db schema created

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const connectDB = require("./config/database");
+const app = express();
+const User = require("./models/user");
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from the server")
-})// Request Handler Function
+connectDB()
+  .then(() => {
+    console.log("Database connection established");
+    app.listen(3000, () => {
+      console.log("server is succesfully Listening on port 3000...");
+    });
+    // Create a server which is listening on port no. 3000
+  })
+  .catch((err) => {
+    console.log("Database cannot be established");
+  });
 
-app.listen(3000,()=>{
-    console.log("server is succesfully Listening on port 3000...")
-}) // Create a server which is listening on port no. 3000
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Shivani",
+    lastName: "Dubey",
+    emailId: "shiv@gmail.com",
+    age: 26,
+    gender: "Female",
+    password:"Shiv@123"
+  });
 
+  try {
+  await user.save();
+  res.send("User Added Succesfully")
+  }
+  catch(err){
+    res.status(400).send("Error saving the user:" +err.message)
+  }
+});
